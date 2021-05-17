b46f3c3e90b39addf0d19a4a7de56f913efb460e
noahlthorne <noahlthorne@gmail.com>
2021-05-17 11:02:33 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "questions#ask"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer", as: :answer_path
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
